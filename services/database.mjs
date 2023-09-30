7b6979506acac17cd5bd59ec074d890423f87cf2
Leon Kipkoech <leonkipkip@gmail.com>
2023-09-30 12:11:13 -0400

    added firebase

--- services/database.mjs
+++ services/database.mjs
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { firebaseConfig } from '../env/firebaseConfig.mjs';
+import { firebaseConfig } from '../env_var/firebaseConfig.mjs';
 
 import dotenv from 'dotenv';
 dotenv.config();
